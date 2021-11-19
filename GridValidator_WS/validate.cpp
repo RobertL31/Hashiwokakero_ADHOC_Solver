1fae7bf5a986121e3c3dc5eeb0ef752e4dfc2f7c
RobertL31 <loicalban@laposte.net>
2021-11-19 23:44:23 +0100

    Start of JSON management

--- GridValidator_WS/validate.cpp
+++ GridValidator_WS/validate.cpp
@@ -1,36 +1,37 @@
 #include "pistache/endpoint.h"
 #include "json.hpp"
 #include <iostream>
+#include <string>
 
 using namespace std;
 using namespace Pistache;
 
 using json = nlohmann::json;
 
-class HelloHandler : public Http::Handler
+class BasicHandler : public Http::Handler
 {
 public:
-    HTTP_PROTOTYPE(HelloHandler)
+    HTTP_PROTOTYPE(BasicHandler)
 
-    void onRequest(const Http::Request& /*request*/, Http::ResponseWriter response) override
+    void onRequest(const Http::Request& request, Http::ResponseWriter response) override
     {
-        response.send(Pistache::Http::Code::Ok, "Hello World\n");
+        json json_request = json::parse(request.body());
+        cout << "received json " << json_request << endl;
+        json_request["param0"] = 1;
+        response.send(Pistache::Http::Code::Ok, json_request.dump(3));
     }
 };
 
 int main()
-{
-    json j;
-    j["coucou"] = 3;
-    
+{   
     Pistache::Address addr(Pistache::Ipv4::any(), Pistache::Port(50500));
     auto opts = Pistache::Http::Endpoint::options()
                     .threads(1);
 
     Http::Endpoint server(addr);
     server.init(opts);
-    server.setHandler(Http::make_handler<HelloHandler>());
+    server.setHandler(Http::make_handler<BasicHandler>());
     
-    cout << "Server listening on PORT 50500 : " << j["coucou"] << endl;
+    cout << "Server listening on PORT 50500 : " << endl;
     server.serve();
 }
