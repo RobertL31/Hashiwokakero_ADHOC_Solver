8b544cd82d21d5c858c36280369ba0536e9d07ab
RobertL31 <loicalban@laposte.net>
2021-11-30 15:45:55 +0100

    [ADD] [TBT] Working GridValidator

--- GridValidator_WS/validate.cpp
+++ GridValidator_WS/validate.cpp
@@ -2,20 +2,49 @@
 #include "json.hpp"
 #include <iostream>
 #include <string>
+#include <set>
+#include <queue>
 
 using namespace std;
 using namespace Pistache;
 using json = nlohmann::json;
 
 struct Node_t{
-    int nLinks;
-    int* links;
+    set<int> links;
     bool marked;
 };
 
-Node_t* JsonToNodeArray(json& jsonInput){
-    //TODO
-    return nullptr;
+void JsonToNodeArray(json& jsonInput, Node_t* nodeArray){
+    
+    int i = 0;
+    for(auto& jsonIsland : jsonInput){
+        set<int> a;
+        jsonIsland["connections"].get_to(a);
+        nodeArray[i++] = {.links = a, .marked = false};
+    }
+}
+
+
+int ExploreNodeArray(Node_t* nodeArray){
+
+    int checked = 0;
+    queue<Node_t*> toCheck;
+    toCheck.push(&nodeArray[0]);
+
+    while(toCheck.size() != 0){
+        Node_t* actual = toCheck.front();
+        if( !actual->marked){
+            for(const int& next : actual->links){
+                //cout << next << endl;
+                toCheck.push(&nodeArray[next]);
+            }
+            actual->marked = true;
+            ++checked;
+        }
+        toCheck.pop();
+    }
+
+    return checked;
 }
 
 
@@ -26,6 +55,9 @@ public:
 
     void onRequest(const Http::Request& request, Http::ResponseWriter response) override
     {
+
+        cout << "Hey" << endl;
+
         if(request.method() != Http::Method::Get){
             response.send(Pistache::Http::Code::Bad_Request, "Only GET operations with JSON body are accepted\n");
             return;
@@ -41,10 +73,39 @@ public:
 
 
         json jsonIslands = jsonInput["grid"]["islands"];
-        Node_t* node_array = JsonToNodeArray(jsonIslands);
+
+        // Check if connections number is the same as population for every island   
+        bool check = true;
+        for(auto& jsonIsland : jsonIslands){
+            if(jsonIsland["population"].get<int>() != jsonIsland["connections"].size()){
+                check = false;
+                //cout << "Island " << jsonIsland["uuid"] << " is not valid !" << endl;
+                break;
+            } else {
+                //cout << "Island " << jsonIsland["uuid"] << " has " << jsonIsland["population"].get<int>() << " needs and " << jsonIsland["connections"].size() << " connection !" << endl;
+            }
+        }
+
+
+        // Check if all island can reach others
+        Node_t* nodeArray = new Node_t[jsonIslands.size()];
+        JsonToNodeArray(jsonIslands, nodeArray);
+
+        /*
+        for(int i=0; i<jsonIslands.size(); ++i){
+           cout << "ISLAND " << i << "[" << endl; 
+           for(const int& el : nodeArray[i].links){
+               cout << el << ";" << endl;
+           }
+           cout << "MARKED " << nodeArray[i].marked << endl;
+        }
+        */
+
+        //cout << "size : " << jsonIslands.size() << endl;
+        check = check && (ExploreNodeArray(nodeArray) == jsonIslands.size());
 
         //cout << "received json :\n" << json_input.dump(4) << endl;
-        response.send(Pistache::Http::Code::Ok, "Your request body was :\n" + jsonInput.dump(4));
+        response.send(Pistache::Http::Code::Ok, check ? "0" : "1" );
     }
 };
 
